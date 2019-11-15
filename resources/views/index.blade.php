7596470c4e76d687947701dff62ad6e197cad771
DimkaSF <fomidiman@gmail.com>
2019-11-15 14:26:20 +0300

    everything ready

---
+++ resources/views/index.blade.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CssFirst</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="{{asset("styles/styles.css")}}" type="text/css" />
+        <link rel="stylesheet" href="{{asset("styles/styles.media.css")}}" type="text/css" />
+    </head>
+    <body>
+        <div class="bodyWrapper">
+            <div class="headerWrapper">
+                <div class="menu">
+                    <div class="item act">Home</div>
+                    <div class="item">Portfolio</div>
+                    <div class="item">About us</div>
+                    <div class="item">Contact</div>
+                </div>
+            </div>
+            <div class="contentWrapper">
+                <div class="firstWrapper contentPadding">
+                    <div class="left">
+                        <h1>OUR STRONG ORGANIZATION</h1>
+                        <span>
+                            Lorem ipsum dolor sit amet, consectetur adipis cing elit,
+                            sed do eiusmod <br/> tempor incididunt  ut labore et. ectetur adig ipis cing elit, sed do eiusmod <br/> tempor incididunt.
+                        </span>
+                        <div class="space50"></div>
+                        <button>Contact Us</button>
+                    </div>
+                    <div class="right">
+                        <div class="videoContainer">
+                            <video height="100%" width="100%"></video>
+                        </div>
+                    </div>
+                </div>
+                <div class="contentPadding secondWrapper">
+                    <div class="left">
+                        <h2>DO YOU KNOW WE CAN PROVIDE FOR YOU ?</h2>
+                         <span>
+                            Lorem ipsum dolor sit amet, consectetur adipis cing elit,
+                            sed do eiusmod tempor incididunt ut labore et.
+                            Lorem ni ipsum dolor sit amet, consectetur adipis
+                            cing elit, ed doi eiusmod tempor incididunt ut labore et.
+                        </span>
+                        <div class="space50"></div>
+                        <button>Learn More</button>
+                    </div>
+                    <div class="right">
+                        <div class="item">
+                            <img src="{{asset("img/Settings.png")}}" />
+                            <h3>Managment</h3>
+                            <span>
+                                Lorem ipsum dolor sitea amet, zixf conseit  adipi cing elit, seddi do eiusmod btdempor in didunt utlae ore etioeew idolor sit amet,
+                            </span>
+                        </div>
+                        <div class="item">
+                            <img src="{{asset("img/Pencil.png")}}" />
+                            <h3>UI / UX Design</h3>
+                            <span>
+                                Lorem ipsum dolor sitea amet, zixf conseit  adipi cing elit, seddi do eiusmod btdempor in didunt utlae ore etioeew idolor sit amet,
+                            </span>
+                        </div>
+                        <div class="item">
+                            <img src="{{asset("img/Diamond.png")}}" />
+                            <h3>Logo / Branging</h3>
+                            <span>
+                                Lorem ipsum dolor sitea amet, zixf conseit  adipi cing elit, seddi do eiusmod btdempor in didunt utlae ore etioeew idolor sit amet,
+                            </span>
+                        </div>
+                        <div class="item">
+                            <img src="{{asset("img/Truck 2.png")}}" />
+                            <h3>Animation</h3>
+                            <span>
+                                Lorem ipsum dolor sitea amet, zixf conseit  adipi cing elit, seddi do eiusmod btdempor in didunt utlae ore etioeew idolor sit amet,
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="delimiter contentPadding"></div>
+                <div class="contentPadding thirdWrapper">
+                    <h2>FEATURE PRODUCTS</h2>
+                    <div class="subhead">
+                        <span>Lorem ipsum dolor sit amet, consectetur adipis cing elit,sed do eiusmod tempor incididunt ut labore et.</span>
+                    </div>
+                    <div class="products">
+                        <div class="item act">ALL</div>
+                        <div class="item">PRINT TEMPLATE</div>
+                        <div class="item">WEB TEMPLATE</div>
+                        <div class="item">USER INTERFACE</div>
+                        <div class="item">MOCK-UP</div>
+                    </div>
+
+                    <div class="productsPreview">
+                        <div class="item" >
+                            <img src="/img/rect_gray.png" width="100%">
+                        </div>
+                        <div class="item" >
+                            <img src="/img/rect_gray.png" width="100%">
+                        </div>
+                        <div class="item" >
+                            <img src="/img/rect_gray.png" width="100%">
+                        </div>
+                        <div class="item" >
+                            <img src="/img/rect_gray.png" width="100%">
+                        </div>
+                        <div class="item" >
+                            <img src="/img/rect_gray.png" width="100%">
+                        </div>
+                        <div class="item" >
+                            <img src="/img/rect_gray.png" width="100%">
+                        </div>
+                        <div class="item" >
+                            <img src="/img/rect_gray.png" width="100%">
+                        </div>
+                        <div class="item" >
+                            <img src="/img/rect_gray.png" width="100%">
+                        </div>
+                    </div>
+                </div>
+                <div class="contentPadding fourthWrapper">
+                    <h2>MEET OUT TEAM</h2>
+                    <div class="subhead">
+                        <span>Lorem ipsum dolor sit amet, consectetur adipis cing elit,
+                            sed dooing eiusmod tempor incididut labore Ui / Ux , print template.</span>
+                    </div>
+                    <div class="workers">
+                        <div class="selected">
+                            <div class="picture" style="background-image: url({{asset("img/Selected.png")}})">
+                                <img src="/img/i/11.gif" width="100%"/>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <h3>MARK WAUGH</h3>
+                            <span>
+                                Lorem ipsum dolor sit amet, consectetur adipis cing elit,
+                                sed dooing eiusmod tempor incididut labore Ui / Ux , print template.
+                            </span>
+                            <div class="socials">
+                                <a class="item" href="#">FACEBOOK</a>
+                                <a class="item" href="#">VK</a>
+                                <a class="item" href="#">INSTA</a>
+                                <a class="item" href="#">GMAIL</a>
+                            </div>
+                            <div class="other">
+                                <div class="person">
+                                    <img src="{{asset("img/face.png")}}" />
+                                    <div class="name">First Name</div>
+                                </div>
+                                <div class="person">
+                                    <img src="{{asset("img/face.png")}}" />
+                                    <div class="name">First Name</div>
+                                </div>
+                                <div class="person">
+                                    <img src="{{asset("img/face.png")}}" />
+                                    <div class="name">First Name</div>
+                                </div>
+                                <div class="person">
+                                    <img src="{{asset("img/face.png")}}" />
+                                    <div class="name">First Name</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="contentPadding fifthWrapper">
+                    <h2>Design tips, tricks, and freebies. Delivered weekly.</h2>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod.</span>
+
+                    <form>
+                        <input name="email" placeholder="Email Address..." />
+
+                        <button type="submit">SUBSCRIBE</button>
+                    </form>
+                </div>
+
+                <div class="sixthWrapper">
+                    <div class="left">
+                        <h2>GET IN TOUCH</h2>
+                        <div class="subhead">
+                            <span>
+                                Lorem ipsum dolor sit amet, consectetur adipis cing elit,
+                                sed do eiusmod tempor incididunt ut labore et.ur adipis
+                                cing elit, sed do eiusmod tempor incididunt ut labore et.
+                                ur adipis cing elit, sed do eiusmod tempor incididunt ut labore et.
+                            </span>
+                        </div>
+                        <div class="contacts">
+                            <div class="form">
+                                <form>
+                                    <input name="name" type="text" placeholder="Name"/>
+                                    <input name="email" type="text" placeholder="Your mail"/>
+                                    <textarea placeholder="Type your message" rows="12"></textarea>
+
+                                    <button>SEND MESSSAGE</button>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="right">
+                        <div class="info">
+                            <h4>CONTACT INFO</h4>
+                            <span>Jalalabad 24 / A, Ambaarkhana, Sylhet, <br />Bangladesh</span>
+                            <span>redwan@deviserweb.com</span>
+                            <span>(+88) 017 617 46373</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="footer">
+                <div class="contentPadding footerWrapper">
+                    <div class="left">© Copyright @Junaed 2016, All rights reserved.</div>
+                    <div class="right">
+                        <img class="item" src="{{asset("img/fb.png")}}" />
+                        <img class="item" src="{{asset("img/twitter.png")}}" />
+                        <img class="item" src="{{asset("img/linkedin.png")}}" />
+                        <img class="item" src="{{asset("img/google.png")}}" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
